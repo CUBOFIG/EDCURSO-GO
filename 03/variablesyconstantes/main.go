45d8890cc16ea332beaa5054eb2c18d8c69a5ce0
Heriberto Figueroa Michel <hfigueroa@ucol.mx>
2020-02-03 14:29:03 -0600

    primer commit

---
+++ 03/variablesyconstantes/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name, lastname := "Heriberto", "Figueroa"
+
+	fmt.Println(name)
+
+	name, edad := "Maaria", 20
+
+	fmt.Println(name, lastname, edad)
+}
