a67c6c472418dcf2d42d0ff5ad06ecb122dc0be1
Heriberto Figueroa Michel <hfigueroa@ucol.mx>
2020-02-27 00:34:39 -0600

    comentarios, constantes y variables

--- 03/variablesyconstantes/main.go
+++ 03/variablesyconstantes/main.go
@@ -3,12 +3,18 @@ package main
 import "fmt"
 
 func main() {
-
+	const idioma = "Español"
 	name, lastname := "Heriberto", "Figueroa"
 
 	fmt.Println(name, lastname)
 
 	name, edad := "Maaria", 20
 
-	fmt.Println(name, lastname, edad)
+	fmt.Println(name, lastname, edad, idioma)
 }
+
+//TIPICOS COMENTARIOS
+
+/*
+	TIPICOS COMENTARIOS
+*/
